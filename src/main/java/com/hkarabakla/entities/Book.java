eb14028490b3a4afa89e3dd7d3abfb207f7133c7
sahindogukan <53344451+sahindogukan@users.noreply.github.com>
2021-01-23 22:52:37 +0300

    Update Book.java

--- src/main/java/com/hkarabakla/entities/Book.java
+++ src/main/java/com/hkarabakla/entities/Book.java
@@ -1,14 +1,37 @@
 package com.hkarabakla.entities;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Set;
 
 @Entity
 public class Book {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String isbn;
 
     private String name;
+
+    private String description;
+
+    private LocalDate publishedDate;
+
+    private LocalDate addedDate;
+
+    private double price;
+
+    private String currency;
+
+    private String imageUrl;
+
+    @ManyToMany
+    @JoinTable(
+            name = "book_author",
+            joinColumns = @JoinColumn(name = "book_isbn"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
+
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "category_id", referencedColumnName = "id")
@@ -30,6 +53,62 @@ public class Book {
         this.name = name;
     }
 
+    public void setAddedDate(LocalDate addedDate) {
+        this.addedDate = addedDate;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(LocalDate publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public LocalDate getAddedDate() {
+        return addedDate;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
     public Category getCategory() {
         return category;
     }
@@ -43,6 +122,14 @@ public class Book {
         return "Book{" +
                 "isbn='" + isbn + '\'' +
                 ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", publishedDate=" + publishedDate +
+                ", addedDate=" + addedDate +
+                ", price=" + price +
+                ", currency='" + currency + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", authors=" + authors +
+                ", category=" + category +
                 '}';
     }
 }
